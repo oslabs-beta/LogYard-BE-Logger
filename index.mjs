99deed6f193fa2d95af0e0785147690c78f7110e
Geoff Sun <geoffsun2@gmail.com>
2023-09-20 14:38:21 -0400

    updated to backend logger

--- index.mjs
+++ index.mjs
@@ -1,17 +1,45 @@
-import postLog from '../postLog.js';
+import { createLogger, format, transports }  from 'winston';
+import 'winston-mongodb';
+import getConfig from './getConfig.js';
 
-
-const createLog = async (level, message, Context = {}) => {
+const logger = () => {
+  const config = getConfig();
+  console.log('config in func', config)
+  return config;
   
-  const params = {
-    level,
-    message,
-      meta : {
-        Context
-      }
-  }
-  
-  postLog(params);
-};
+}
+const config = logger();
+console.log('config out of func', config)
 
-export default createLog; 
+export default createLogger({
+  transports: [
+    /* !LOGGER TEMPLATE!
+
+    _id: AUTO GENERATED
+    timestamp: AUTO GENERATED
+    level: USER DECLARED ( (0)error, (1)warn, (2)info, (3)http, (4)verbose, (5)debug, (6)silly )
+    message: USER DECLARED
+    meta: {
+      Context1: USER DECLARED
+      Context2: USER DECLARED
+    }
+
+    */
+
+    new transports.MongoDB({
+      // to collect all level types in DB, put this as 'silly'. Anything at or below the indicated level here will be placed in the database.
+      level: config.level, 
+      db: config.uri,
+      options: {
+        useUnifiedTopology: true,
+      },
+      // dbName and collection MUST match the location at which the mongo server is pointing to in db.js!
+      dbName: 'your_database_name',
+      collection: 'logs',
+      format: format.combine(
+        format.json(),
+        format.metadata()
+      )
+    })
+  ]
+});
